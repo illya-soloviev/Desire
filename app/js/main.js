1d72ce4ffad74fb6ae6f8f62c2767e11ba0b1d25
Illya <ilya.solovev.88@bk.ru>
2023-10-28 20:50:55 +0300

    added the block of top

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,18 @@
 $(function () {
 
     $('.header__btn').on('click', () => {
-        $('.rightside-menu').removeClass('rightside-menu--close');
+        $('.rightside-menu').addClass('rightside-menu--show');
     });
 
     $('.rightside-menu__close').on('click', () => {
-        $('.rightside-menu').addClass('rightside-menu--close');
+        $('.rightside-menu').removeClass('rightside-menu--show');
+    });
+
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true
     });
 
 })
