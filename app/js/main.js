b16214503f0b53041c5ceec25dc8f1c805f77897
Illya <ilya.solovev.88@bk.ru>
2023-11-05 18:14:41 +0300

    finished the blog__items

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,11 @@ $(function () {
         slidesToScroll: 10
     });
 
+    $('.article-slider__box').slick({
+        prevArrow: '<button class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slider-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slider-right.svg" alt="arrow right"></button>',
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living-room'
